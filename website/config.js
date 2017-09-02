3cba7b3c5958175a242888c35924464681be4566
ZHANGJING\13821 <13821320100@Outlook.com>
2017-09-02 15:56:57 +0800

    Updata Config & website

--- website/config.js
+++ website/config.js
@@ -1,12 +1,12 @@
-var api = "http://poolhost.com:1117";
+var api = "http://poolhost.com:8117";
 
 var poolHost = "poolhost.com";
 
 var irc = "irc.freenode.net/#poolhost";
 
-var email = "support@poolhost.com";
+var email = "13821320100@Outlook.com";
 
-var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-ETH", "{symbol}-CNY"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
